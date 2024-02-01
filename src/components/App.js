aaddc42d41d26beb91269db8d5737ddf13c07d7a
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-01 06:26:54 +0000

    sol1

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const render=()=>{
+    let p=document.createElement("p");
+    p.setAttribute("id","para");
+    p.textContent="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    let main=document.getElementById("main");
+    main.appendChild(p);  
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="Click" onClick={render}>Click</button>
     </div>
   );
 }
